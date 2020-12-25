1344cc85173f62f3e9a4a52defff788550d10b6c
Wang Yulin <aaron.wangyulin@gmail.com>
2020-12-25 17:11:03 +0800

    add star_ch

--- scrap.py
+++ scrap.py
@@ -1,26 +1,16 @@
-import pandas as pd 
 from bs4 import BeautifulSoup
 import requests
 
-url = 'https://bk.tw.lvfukeji.com/wiki/%E6%98%9F%E5%BA%A7%E5%88%97%E8%A1%A8'
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+def scrap(url):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+    response = requests.get(url=url, headers=headers)
+    if response.status_code == 200:
+        payload = response.content
+        soup = BeautifulSoup(payload, "html.parser")
+        return soup
 
-response = requests.get(url=url, headers=headers)
-payload = response.content
 
-soup = BeautifulSoup(payload, "html.parser")
-table = soup.find("table", class_='wikitable sortable')
-column_names = [th.text for th in table.tbody.tr.find_all('th')]
-
-rows = []
-for tr in table.tbody.find_all('tr')[1:]:
-    row = []
-    for td in tr.find_all('td'):
-        row.append(td.text)
-    rows.append(row)
-
-df = pd.DataFrame(rows, columns=column_names)
-df.columns = ['name_cn', 'abbr', 'name', 'area', 'ra', 'dec', 'quadrant', 'family', 'bs']
-df.set_index('name', inplace=True)
-
-df1 = pd.merge(df, df.bs.str.extract(r'(?P<bs_name_bayer>.*)\((?P<bs_name_cn>.*)\)'), on='name')
+if __name__ == "__main__":
+    url = 'https://www.baidu.com/'
+    soup = scrap(url)
+    soup
---
+++ star_ch.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from scrap import scrap
+
+def star_ch():
+    url = 'https://bk.tw.lvfukeji.com/wiki/%E6%98%9F%E5%BA%A7%E5%88%97%E8%A1%A8'
+    soup = scrap(url)
+    table = soup.find("table", class_='wikitable sortable')
+    column_names = [th.text for th in table.tbody.tr.find_all('th')]
+
+    rows = []
+    for tr in table.tbody.find_all('tr')[1:]:
+        row = []
+        for td in tr.find_all('td'):
+            row.append(td.text)
+        rows.append(row)
+
+    df = pd.DataFrame(rows, columns=column_names)
+    df.columns = ['name_cn', 'abbr', 'name', 'area', 'ra', 'dec', 'quadrant', 'family', 'bs']
+    df.set_index('name', inplace=True)
+    df.bs = df.bs.apply(lambda x: x.strip('\n'))
+    df1 = df.bs.str.extract(r'(?P<bs_name_bayer>.*)\((?P<bs_name_cn>.*)\)')
+    df_ch = pd.merge(df, df1, on='name')
+    return df_ch
+
